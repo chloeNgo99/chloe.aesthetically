29541c2595f7d70d500f95f3fb324227fefc3e34
Chloe Ngo <chloengo1704@gmail.com>
2022-12-28 12:44:41 -0800

    activate social media links

--- src/Components/Media.js
+++ src/Components/Media.js
@@ -3,9 +3,9 @@ import React from "react"
 export default function Media() {
     return (
         <div className="social">
-            <img className="github" src="./images/github.png" />
-            <img className="instagram" src="./images/instagram.png" />
-            <img className="mail" src="./images/mail.png" />
+            <a href="https://github.com/chloeNgo99" target="_blank"><img className="github" src="./images/github.png" /></a>
+            <a href="https://www.instagram.com/chloe.aesthetically" target="_blank"><img className="instagram" src="./images/instagram.png" /></a>
+            <a href="https://www.linkedin.com/in/chloe-ngo-a540b3253/" target="_blank"><img className="linkedin" src="./images/linkedin.png" /></a>
         </div>
 
     )
