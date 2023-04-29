5799ca844eda937d0acf4c249cb2352c44d51863
Chloe Ngo <chloengo1704@gmail.com>
2023-04-29 15:38:22 -0700

    new update

--- src/Components/Update.js
+++ src/Components/Update.js
@@ -6,7 +6,7 @@ export default function Project() {
     return (
         <div className='update' id="updateSection">
            <h2>Check out my new website markup</h2>
-           <img src="./images/markuo.png" alt="markup" href='https://www.figma.com/proto/7u3t1V75xpHH2jNDGTB7ky/Untitled?node-id=1-19&scaling=scale-down&page-id=0%3A1'/>
+           <img src="./images/markup.png" alt="markup" href='https://www.figma.com/proto/7u3t1V75xpHH2jNDGTB7ky/Untitled?node-id=1-19&scaling=scale-down&page-id=0%3A1'/>
         </div>
     )
 
