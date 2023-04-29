6f228168a269fd2c00350258b3635c4b7676bcaf
Chloe Ngo <chloengo1704@gmail.com>
2023-04-29 15:53:40 -0700

    new update

--- src/App.js
+++ src/App.js
@@ -26,10 +26,10 @@ export default function App() {
     let bodyStyle = {};
     bodyStyle.transition = 'background-color 1.5s ease';
     if (scroll < 500) {
-      bodyStyle = { background: 'linear-gradient(to top left, #EAD3E3, #FAC7D3, #D5D8ED)' };
+      bodyStyle = { background: 'linear-gradient(to top left, #F3F9D3, #F3D4E3, #D5DCEA)' };
       bodyStyle.transition = "background 1s ease-out";
     } else if (scroll >= 500 && scroll < 1800) {
-      bodyStyle = { background: 'linear-gradient(to top left, #DFEBF5, #ECE0DC, #F8F3D9)' };
+      bodyStyle = { background: 'linear-gradient(to top left, #DFEBF5, #ECE0DC, #C8E5FD)' };
       bodyStyle.transition = "background 1s ease-out";
     } else {
       bodyStyle = { background: 'linear-gradient(to top left, #C6E2F1, #EADFF5, #EADFF5)' };
--- src/Components/Update.js
+++ src/Components/Update.js
@@ -5,8 +5,8 @@ import Media from "./Media"
 export default function Project() {
     return (
         <div className='update' id="updateSection">
-           <h2>Check out my new website markup</h2>
-           <img src="./images/markup.png" alt="markup" href='https://www.figma.com/proto/7u3t1V75xpHH2jNDGTB7ky/Untitled?node-id=1-19&scaling=scale-down&page-id=0%3A1'/>
+           <h2>Click to visit my new website markup</h2>
+           <a href='https://www.figma.com/proto/7u3t1V75xpHH2jNDGTB7ky/Untitled?node-id=1-19&scaling=scale-down&page-id=0%3A1' target="_blank"><img src="./images/markup.png" alt="markup"/></a> 
         </div>
     )
 
