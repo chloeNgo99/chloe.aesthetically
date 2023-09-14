9591201bbbcf67dae647e8424b8e29e65079f297
Chloe Ngo <chloengo1704@gmail.com>
2023-09-13 22:34:15 -0700

    fix bug

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -44,7 +44,7 @@ const Contact = () => {
         <form
           className="contact_form"
           method="POST"
-          action="https://formsubmit.co/el/gobivi"
+          action="https://formsubmit.co/chloengo1704@gmail.com"
         >
           <div className="contact_form_group">
             <div className="contact_form_div">
