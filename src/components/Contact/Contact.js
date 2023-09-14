434840ca4cd2f840d4bae961d319deeab6af0839
Chloe Ngo <chloengo1704@gmail.com>
2023-09-13 22:09:48 -0700

    fix contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
-  function submitForm(){
+  function submitForm() {
     document.getElementById("contact_form").reset();
   }
   return (
@@ -15,7 +15,7 @@ const Contact = () => {
           <p className="contact_details">Shoot me an email! 😬</p>
         </div>
 
-        <form className="contact_form" method="POST" action="https://formsubmit.co/7057a4e77b5519ef11231adb4a236cd9">
+        {/* <form className="contact_form" method="POST" action="https://formsubmit.co/7057a4e77b5519ef11231adb4a236cd9">
           <div className="contact_form_group">
             <div className="contact_form_div">
               <input type="text" className="contact_form_input" placeholder='Your name'/>
@@ -40,10 +40,56 @@ const Contact = () => {
             </textarea>
           </div>
           <button onclick="submitForm()" type="submit" name="submit" className="btn">SEND!</button>
+        </form> */}
+        <form
+          className="contact_form"
+          method="POST"
+          action="https://formsubmit.co/7057a4e77b5519ef11231adb4a236cd9"
+        >
+          <div className="contact_form_group">
+            <div className="contact_form_div">
+              <input
+                type="text"
+                className="contact_form_input"
+                placeholder="Your name"
+                name="name" // Make sure this matches the field name expected by FormSubmit
+              />
+            </div>
+
+            <div className="contact_form_div">
+              <input
+                type="email"
+                className="contact_form_input"
+                placeholder="Your email"
+                name="email" // Make sure this matches the field name expected by FormSubmit
+              />
+            </div>
+          </div>
+          <div className="contact_form_div">
+            <input
+              type="text"
+              className="contact_form_input"
+              placeholder="Your subject"
+              name="subject" // Make sure this matches the field name expected by FormSubmit
+            />
+          </div>
+
+          <div className="contact_form_div contact_form_area">
+            <textarea
+              name="message" // Make sure this matches the field name expected by FormSubmit
+              cols="30"
+              rows="10"
+              className="contact_form_input"
+              placeholder="Your message"
+            ></textarea>
+          </div>
+          <button type="submit" name="submit" className="btn">
+            SEND!
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
